d5573e2165df31b74486deaa15bb47c578dcbd9f
Go-Gunji <go.eliteforce@gmail.com>
2022-01-19 23:30:52 +0900

    add/#8 AnswersListコンポーネント追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import defaultDataset from './dataset';
-import './assets/styles/style.css'
+import './assets/styles/style.css';
+import { AnswersList } from './components';
 
 class App extends React.Component {
   constructor(props) {
@@ -17,7 +18,7 @@ class App extends React.Component {
     return (
       <section className="c-section">
         <div className="c-box">
-          {this.state.currentId}
+          <AnswersList answers={this.state.answers} />
         </div>
       </section>
     );
